652196d5468eae11848c09f8bfeb38a898c1b012
GodCm <wz920676274wz@163.com>
2018-12-05 21:01:36 +0800

    First App commit

--- App/Cosmetic/app/urls.py
+++ App/Cosmetic/app/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from app import views
+# from App.Cosmetic.app import views
 
 urlpatterns = [
     url(r'^index/', views.index,name="index"),
--- App/Cosmetic/app/views.py
+++ App/Cosmetic/app/views.py
@@ -1,15 +1,18 @@
 import hashlib
 import sqlite3
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from app.models import UserModel
 
-
-# 首页
+#首页
 def index(request):
-    data = {}
+    data = {
+        'code':200,
+        'title':'首页',
+        'is_login':False,
+    }
     userid = request.session.get("user_id")
     users = UserModel.objects.filter(pk=userid)
     if users.exists():
@@ -17,21 +20,23 @@ def index(request):
         data["username"] = user.username
         data["icon"] = '/static/uploadfiles/' + user.icon.url
         data['is_login'] = True
+    return JsonResponse(data)
 
-    return render(request, "index.html", context=data)
 
-
-# 密码加密
+#密码加密
 def make_pwd(password):
     pwd = str(password)
     password = hashlib.md5(pwd.encode("utf8")).hexdigest()
     return password
 
-
-# 注册
+#注册
 def register(request):
+    data = {
+        'title':'注册',
+    }
     if request.method == "GET":
-        return render(request, "user/register.html")
+        data['code'] = 201
+        return HttpResponse(data)
     elif request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
@@ -43,21 +48,28 @@ def register(request):
         user = UserModel()
         users = UserModel.objects.filter(username=username)
         if users.exists():
-            return HttpResponse("该用户名已被注册!!!")
+            data['code'] = 403
+            data['message'] = '用户名被占用！'
+            return JsonResponse(data)
         else:
             user.username = username
             user.password = make_pwd(password)
             user.email = email
             user.icon = icons
-            request.session["user_id"] = user.id
+            # request.session["user_id"] = user.id
             user.save()
+            data['code'] = 200
+            data['message'] = '注册成功!'
     return redirect(reverse("app:index"))
 
-
-# 登录
+#登录
 def login(request):
+    data = {
+        'title':'登录',
+    }
     if request.method == "GET":
-        return render(request, "user/login.html")
+        data['code'] = 201
+        return JsonResponse(data)
     elif request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
@@ -69,21 +81,26 @@ def login(request):
 
             if user.password == make_pwd(password):
                 request.session['user_id'] = user.id
+                data['code'] = 200
                 return redirect(reverse('app:index'))
-            return HttpResponse("密码输入有误")
+            data['code'] = 405
+            return JsonResponse(data)
+        data['code'] = 403
+        data['message'] = '该用户不存在'
+        return JsonResponse(data)
+    data['code'] = 404
+    return JsonResponse(data)
 
-        return HttpResponse("该用户不存在")
-
-    return HttpResponse("无效请求")
-
-
-# 注销
+#注销
 def loginout(request):
+    data = {
+        'code':200,
+        'is_login':False,
+    }
     request.session.flush()
     return redirect(reverse('app:index'))
 
-
-# 修改个人信息
+#修改个人信息
 def user_info(request):
     data = {
         'title': '信息修改',
@@ -92,14 +109,15 @@ def user_info(request):
     data['user_id'] = userid
     if request.method == 'GET':
         if userid:
+            data['code'] = 202
             user = UserModel.objects.get(pk=userid)
             data['username'] = user.username
             data['email'] = user.email
-            if user.icon:
+            if user.icon :
                 data['icon'] = '/static/uploadfiles/' + user.icon.url
             else:
                 data['icon'] = None
-            return render(request, 'user/userinfo_mod.html', context=data)
+            return JsonResponse(data)
         else:
             return redirect(reverse('app:login'))
     elif request.method == 'POST':
@@ -112,19 +130,19 @@ def user_info(request):
         user.save()
         return redirect(reverse('app:index'))
 
-
-# 商品展示页
+#商品展示页
 def goods_index(request):
     data = {
-        'title': '全部商品'
+        'title':'全部商品'
     }
     userid = request.session.get('user_id')
     users = UserModel.objects.filter(pk=userid)
     if users.exists():
+        data['code'] = 200
         user = users.first()
         data["username"] = user.username
         data["icon"] = '/static/uploadfiles/' + user.icon.url
         data['is_login'] = True
-        return render(request, 'goods/goods1_index.html', context=data)
+        return JsonResponse(data)
     else:
         return redirect(reverse('app:login'))
